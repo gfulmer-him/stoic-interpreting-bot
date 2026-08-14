14a7f0f1a60dcf18da60fb3bfca36433571ea163
gfulmer-him <gavinfulmer@gmail.com>
2026-08-14 11:10:17 -0400

    Update main.py
    added a check of previous quotes

--- src/main.py
+++ src/main.py
@@ -3,7 +3,51 @@ import os
 from datetime import datetime
 from google import genai
 
-def generate_stoic_reflection():
+def fetch_and_check_quote(recent_quotation_file = "recently_quoted.txt"):
+    if not os.path.exists(recent_quotation_file):
+        open(recent_quotation_file, "w").close()
+    with open(recent_quotation_file, "r") as f:
+        recent_quotes = [line.strip() for line in f.readlines() if line.strip()]
+    # Fetch API from GitHub secrets
+    api_key = os.environ.get("LLM_API_KEY")
+    if not api_key:
+        print("Error: LLM_API_KEY environment variable not found.")
+        return
+    # Initialize Gemini client
+    client = genai.Client(api_key=api_key)
+    # Craft prompt
+    history_string = "\n".join(recent_quotes) if recent_quotes else "None"
+    prompt = (
+        f"Find a brief daily Stoic quote from Marcus Aurelius, Seneca, or Epictetus. "
+        f"CRITICAL: It must NOT be identical or highly similar to any quote in this list:\n"
+        f"{history_string}\n\n"
+        f"Return ONLY the quote text and the author name. Do not add any conversational intro or outro text."
+    )
+    # Request a response
+    test_quotation = client.models.generate_content(
+        model = 'gemini-3.6-flash',
+        contents = prompt,
+        )
+    quotation_text = test_quotation.text.strip()
+    recent_quotes.append(quotation_text)
+    if len(recent_quotes) > 10:
+        recent_quotes.pop(0)  # Keep only the last 10 entries
+    with open(recent_quotation_file, "w") as f:
+        for q in recent_quotes:
+            f.write(f"{q}\n")    
+    return quotation_text        
+      
+'''  
+  try:
+        response = requests.get("https://themotivate365.com") # Example API
+        data = response.json()
+        return f'"{data["quote"]}" — {data["author"]}'
+    except Exception as e:
+        print(f"Error fetching quote: {e}")
+        sys.exit(1)
+'''
+
+def generate_stoic_reflection(stoic_quotation):
   # Fetch API from GitHub secrets
   api_key = os.environ.get("LLM_API_KEY")
   if not api_key:
@@ -13,8 +57,9 @@ def generate_stoic_reflection():
   client = genai.Client(api_key=api_key)
   # Craft prompt
   prompt = (
-     "Act as a modern Stoic philosopher. Provide a brief daily Stoic quote "
-     "from Marcus Aurelius, Seneca, or Epictetus. Follow it with a 3-sentence "
+     f"Act as a modern Stoic philosopher who wants to share an interpretion of the following quotation: "
+     f"{stoic_quotation} with a general audience. "
+     f"Restate the quotation and, if necessary, add who the author is. Then follow that with a 3-sentence "
      "practical explanation and one actionable exercise for today. "
      "Keep the tone grounded and clear."
   )
